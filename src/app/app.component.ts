5979b3ed9f346cea2ecebe8c80d97fa794a80b34
mateosepulveda <mateo.sepulveda@usach.cl>
2024-02-01 00:27:45 -0300

    ADD: Substantial CSS improvements and refinement

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,47 +1,54 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'portfolio';
-  menuWidth = '0%' ;
-  shouldCenter = true;
+  menuWidth = '0%';
+  menuTextOpacity = '0';
 
   constructor(private router: Router) {
   }
 
+  ngOnInit(): void {
+    this.router.events
+    .pipe(filter(event => event instanceof NavigationEnd))
+    .subscribe(() => {
+      window.scrollTo(0, 0);
+    });
+  }
+
   showMenu(): void {
     this.menuWidth = '100%';
+    this.menuTextOpacity = '1';
   }
 
   hideMenu(): void {
     this.menuWidth = '0%';
+    this.menuTextOpacity = '0';
   }
 
   navigate(destination: string): void {
     switch (destination) {
       case 'about':
         this.router.navigate(['/about']);
-        this.shouldCenter = true;
         this.hideMenu();
         break;
       case 'projects':
         this.router.navigate(['/projects-list']);
-        this.shouldCenter = false;
         this.hideMenu();
         break;
       case 'contact':
         this.router.navigate(['/contact']);
-        this.shouldCenter = true;
         this.hideMenu();
         break;
       default:
         this.router.navigate(['/about']);
-        this.shouldCenter = true;
         this.hideMenu();
     }
   }
