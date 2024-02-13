2c4b25c0f7610b149a527784811ef67d7f9749f3
mateosepulveda <mateo.sepulveda@usach.cl>
2024-02-13 03:34:44 -0300

    CHG: Redundant part of routes removed, removing hash

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -20,7 +20,7 @@ const routes: Routes = [
 @NgModule({
   declarations: [],
   imports: [
-    RouterModule.forRoot(routes, { useHash: true })
+    RouterModule.forRoot(routes, { useHash: false })
   ],
   exports: [
     RouterModule
--- src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-    production: true,
-    baseHref: 'https://mateosepulveda.github.io/portfolio/',
-};
